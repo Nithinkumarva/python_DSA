6c07da6b398850bf5738120cbac8a51e3c3c9432
Nithin <plaxmipatirr26472@gmail.com>
2026-07-20 12:27:18 +0530

    day1

---
+++ main.py
@@ -0,0 +1,75 @@
+print("Nithin\nNithin")
+print(r"\tcurrent\new\folder")
+# / is an escape character
+# write a pogram to find odd and even numbers with function
+'''def check(num):
+    if num%2==0:
+        print(num,"is even number")
+    else:
+        print(num,"is odd number")
+check(10)
+
+l= [ 1,2,3,4,5]
+l.pop()
+print(l)
+
+dict={
+    "name":"sriram",
+    "geneder":"male",
+    "age":20,
+    "courses":["python","java","Data science"]
+}
+dict["name"]="Nithin"
+dict.update({"course":  ,["python","java","Data science"]})
+print(dict)
+print(dict.keys())
+print(dict.values())
+print(dict.items())
+
+def count(*args):
+    print(type(args))
+# count(1,2,3,4,5)
+def dicts(**kwargs):
+    print(type(kwargs))
+# dicts(name="sriram",gender="male")
+def default(name="sriram",gender="male"):
+    print(name,gender,age,courses)
+def default(geneder,age,name="Nithin"):
+    print(geneder,age,name)
+
+#oops
+l=[1,2,3]
+s="string"
+len(l)
+len(s)
+# 4 pillars of oops
+# 1. Inheritance:
+# 2. Polymorphism:
+# 3. Encapsulation:
+# 4. Abstraction:
+#take a string an input
+# prinmt it back  by removing the fruit and last character of the input string
+x = input("Enter a string: ")
+print(x[1:-1])
+
+#create an exampler while loop which will not terminate , and print n1 in each iteration
+# and how will you stop the pogram manully?
+while True:
+    n1 = int(input("Enter a number: "))
+    print(n1)'''
+
+'''# take a number as input and find the sum of the numbers from 1 to that number by using for loop
+n = int(input("Enter a number: "))
+sum = 0
+for i in range(1,n+1):
+    sum = sum + i
+print(sum)'''
+
+'''# take a number as input and find the sum of thew numbers from 1 to that number by using while loop
+n = int(input("Enter a number: "))
+sum = 0
+i = 1
+while i <= n:
+    sum = sum + i
+    i = i + 1
+print(sum)''''''
